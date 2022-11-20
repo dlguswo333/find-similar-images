4589210c09aef29d87ac04fd7dbdc2668465e7ab
dlguswo333 <dlguswo002@gmail.com>
2022-11-20 13:57:32 +0900

    Init repository

---
+++ src/MainProgram.cs
@@ -0,0 +1,8 @@
+﻿class MainProgram
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length > 0 ? string.Join(" ", args): "Hello world!");
+    }
+}
