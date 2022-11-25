36612cc64c9148893a43edf8a59eb321c43dd622
dlguswo333 <dlguswo002@gmail.com>
2022-11-25 23:11:30 +0900

    Add CommandLineParser package and improve MainProgram

--- src/MainProgram.cs
+++ src/MainProgram.cs
@@ -1,8 +1,98 @@
-﻿class MainProgram
-{
-    static void Main(string[] args)
-    {
-        // Display the number of command line arguments.
-        Console.WriteLine(args.Length > 0 ? string.Join(" ", args): "Hello world!");
+﻿using OpenCvSharp;
+using CommandLine;
+
+public enum Thresholds {
+    low,
+    medium,
+    high
+}
+
+class MainProgram {
+    const string defaultComparator = "mse";
+    static int Main(string[] args) {
+        var parsedArgs = Parser.Default.ParseArguments<Arguments>(args);
+        if (parsedArgs.Errors.Any()) {
+            return -1;
+        }
+        var path1 = parsedArgs.Value.Path1;
+        var path2 = parsedArgs.Value.Path2;
+        var comparator = InitComparator(parsedArgs.Value.Comp ?? defaultComparator);
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        if (path2 == null) {
+            var path = path1;
+            if (!Directory.Exists(path)) {
+                Console.WriteLine($"Given path does not exist or cannot be accessed: {path}");
+                return -1;
+            }
+            var imgPaths = ImgGetter.GetImgPaths(path);
+            var exitFlag = ConsoleLogNumImgs(imgPaths.Length, path);
+            if (exitFlag) {
+                return 0;
+            }
+
+            Console.WriteLine($"Reading {imgPaths.Length} images in memory...");
+            var imgs = new Mat[imgPaths.Length];
+            for (int i = 0; i < imgPaths.Length; ++i) {
+                try {
+                    imgs[i] = Cv2.ImRead(imgPaths[i], ImreadModes.Color);
+                } catch (Exception e) {
+                    Console.WriteLine($"Failed to read a image: {imgPaths[i]}");
+                    Console.WriteLine(e.ToString());
+                    return -1;
+                }
+            }
+
+            Console.WriteLine($"Comparing {imgPaths.Length} images...");
+            for (int i = 0; i < imgPaths.Length; ++i) {
+                var img1 = imgs[i];
+                for (int j = i + 1; j < imgPaths.Length; ++j) {
+                    var img2 = imgs[j];
+                    var similarity = comparator.Compare(img1, img2);
+                    if (comparator.IsSimilar(similarity, Thresholds.high)) {
+                        Console.WriteLine($"{imgPaths[i]} {imgPaths[j]} {similarity}");
+                    }
+                }
+            }
+        }
+
+        watch.Stop();
+        Console.WriteLine($"{watch.ElapsedMilliseconds} ms elapsed.");
+        return 0;
+    }
+
+    private static bool ConsoleLogNumImgs(int numImgs, string path) {
+        if (numImgs == 0) {
+            Console.WriteLine($"No image exists in the given path: {path}");
+            return true;
+        }
+        if (numImgs == 1) {
+            Console.WriteLine($"Single image file has been found in the given path: {path}");
+            return true;
+        }
+        return false;
+    }
+
+    private static IComparator InitComparator(string compSpecifier) {
+        if (compSpecifier == "ncc") {
+            return new NccComparator();
+        }
+        return new MseComparator();
+    }
+
+    class Arguments {
+        [Value(0, Required = true, MetaName = "Path1", HelpText = "Target Path. If you specify one paths, compare images in the same directory.")]
+        public string Path1 { get; set; } = string.Empty;
+
+        [Value(1, Required = false, MetaName = "Path2", HelpText = "Target Path. If you specify two paths, compare images to those from the other directories.")]
+        public string? Path2 { get; set; }
+
+        // Define the bool nullable so that it can be true by default.
+        // https://github.com/commandlineparser/commandline/wiki/CommandLine-Grammar#named-option-types
+        [Option('r', "recursive", Default = true, Required = false, HelpText = "Include images in subdirectories.")]
+        public bool? Recursive { get; set; }
+
+        [Option('c', "comp", Default = defaultComparator, Required = false, HelpText = "Specify comparator. Possible values: mse, ncc.")]
+        public string? Comp { get; set; }
     }
 }
