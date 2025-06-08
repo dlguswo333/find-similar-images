e864d853d2fc5cb82f4d2d9fbfb3ebb3f9250e03
dlguswo333 <dlguswo002@gmail.com>
2025-06-08 12:40:43 +0900

    [#27] Add compare different images test

--- test/SimilarityTest.cs
+++ test/SimilarityTest.cs
@@ -20,15 +20,28 @@ public class SimilarityTest
         Assert.True(comparator.IsSimilar(similarity, FSI.Thresholds.high));
     }
 
+    private static void CompareDifferentImgs(FSI.IComparator comparator) {
+        var img1Path = Path.Join([TestImgFolderPath, "shapes0.jpg"]);
+        var img2Path = Path.Join([TestImgFolderPath, "shapes1.jpg"]);
+        Assert.True(File.Exists(img1Path));
+        Assert.True(File.Exists(img2Path));
+
+        var imgs = FSI.MainProgram.ReadAndProcessImgs([img1Path, img2Path], comparator.ProcessImg);
+        var similarity = comparator.Compare(imgs[0], imgs[1]);
+        Assert.False(comparator.IsSimilar(similarity, FSI.Thresholds.low));
+    }
+
     [Fact]
     public void TestMseComparator() {
         var comparator = new FSI.MseComparator();
         CompareIdenticalImgs(comparator);
+        CompareDifferentImgs(comparator);
     }
 
     [Fact]
     public void TestNccComparator() {
         var comparator = new FSI.NccComparator();
         CompareIdenticalImgs(comparator);
+        CompareDifferentImgs(comparator);
     }
 }
